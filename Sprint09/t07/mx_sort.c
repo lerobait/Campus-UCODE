7b94e41de2bdc6be809994e9b9d7f9b03c96319d
Нікулін Артем Дмитрович <48065996+lerobait@users.noreply.github.com>
2023-02-09 12:00:29 +0200

    Sprint09

--- Sprint09/t07/mx_sort.c
+++ Sprint09/t07/mx_sort.c
@@ -3,12 +3,10 @@
 void mx_sort(int *arr, int size, bool (*f)(int, int));
 
 void mx_sort(int *arr, int size, bool (*f)(int, int)) {
-    int j, g, t;
-
-    for(j = 0; j < size - 1; j++) {
-        for(g = 0; g < size - 1; g++) {
+    for(int j = 0; j < size - 1; j++) {
+        for(int g = 0; g < size - 1; g++) {
             if(f(arr[g], arr[g + 1])) {
-                t = arr[g];
+                int t = arr[g];
                 arr[g] = arr[g + 1];
                 arr[g + 1] = t;
             }
