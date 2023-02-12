afe9478b7d5f545de3816469ee9bc2e1d76c430c
Нікулін Артем Дмитрович <48065996+lerobait@users.noreply.github.com>
2023-02-12 17:32:35 +0200

    Race01

--- Race01/src/mx_equasion_1_var.c
+++ Race01/src/mx_equasion_1_var.c
@@ -119,17 +119,21 @@ void mx_equasion_1_var(char* operand1, char* operand2, char *result_str, char** 
         }
     }
 
-    if (*operation == '/') {
+    if(*operation == '/') {
         if (num2 == 0) {
             mx_printerr("Invalid operation: ");
                 mx_printerr(argv[3]);
                 mx_printerr("\n");
                 exit(-1);
         }
+    int j = 0;
+        
+    while(j < 11) {
         if (c == 0) {
-            num1 = result * num2;
+            num1 = result * num2 + j;
             if (num1 >= mx_pow(10, mx_strlen(operand1) - 1) && num1 < mx_pow(10, mx_strlen(operand1))) {
-                mx_print_result(num1, num2, result, operation);     
+                mx_print_result(num1, num2, result, operation); 
+                j++;
             }
             else {
                 mx_printerr("Invalid operation: ");
@@ -138,29 +142,30 @@ void mx_equasion_1_var(char* operand1, char* operand2, char *result_str, char** 
                 exit(-1);
             }
         }
-        else if (c == 1) {
-            num2 = num1 / result;
-            if (num2 >= mx_pow(10, mx_strlen(operand2) - 1) && num2 < mx_pow(10, mx_strlen(operand2))) {
-                mx_print_result(num1, num2, result, operation);         
-            }
-            else {
-                mx_printerr("Invalid operation: ");
-                mx_printerr(argv[3]);
-                mx_printerr("\n");
-                exit(-1);
-            }
+    }
+    if (c == 1) {
+        num2 = num1 / result;
+        if (num2 >= mx_pow(10, mx_strlen(operand2) - 1) && num2 < mx_pow(10, mx_strlen(operand2))) {
+            mx_print_result(num1, num2, result, operation);         
         }
-        else if (c == 2) {
-            result = num1 / num2;
-            if (result >= mx_pow(10, mx_strlen(result_str) - 1) && result < mx_pow(10, mx_strlen(result_str))) {
-                mx_print_result(num1, num2, result, operation);         
-            }   
-            else {
-                mx_printerr("Invalid operation: ");
-                mx_printerr(argv[4]);
-                mx_printerr("\n");
-                exit(-1);
-            } 
+        else {
+            mx_printerr("Invalid operation: ");
+            mx_printerr(argv[3]);
+            mx_printerr("\n");
+            exit(-1);
         }
     }
+    else if (c == 2) {
+        result = num1 / num2;
+        if (result >= mx_pow(10, mx_strlen(result_str) - 1) && result < mx_pow(10, mx_strlen(result_str))) {
+            mx_print_result(num1, num2, result, operation);         
+        }   
+        else {
+            mx_printerr("Invalid operation: ");
+            mx_printerr(argv[4]);
+            mx_printerr("\n");
+            exit(-1);
+        } 
+    }
+}
 }
