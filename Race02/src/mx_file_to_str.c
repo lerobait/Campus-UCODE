8c37167849bccbf7f9e0d3a73da84acbf12ae870
Нікулін Артем Дмитрович <48065996+lerobait@users.noreply.github.com>
2023-02-16 13:30:36 +0200

    Race02

--- Race02/src/mx_file_to_str.c
+++ Race02/src/mx_file_to_str.c
@@ -1,28 +1,35 @@
 #include "header.h"
 
 char *mx_file_to_str(const char *filename) {
-    if(!filename) {
-        return NULL;
-    }
+    char s[1];
+    int c = 0;
     int f = open(filename, O_RDONLY);
-
-    if(f < 0) {
-        return NULL;
-    }
-    int l = 0;
-    char c;
-
-    while(read(f, &c, 1) != 0) {
-        l++;
+    int u = read(f, s, sizeof(s));
+    
+    while(u > 0) {
+        if(s[0] == '.' || s[0] == '#' || s[0] == '\n') {
+            ++c;
+        }
+        u = read(f, s, sizeof(s));
     }
     close(f);
 
     f = open(filename, O_RDONLY);
-    char *t = mx_strnew(l);
+    char *r = (char *)malloc(sizeof(char) * c + 1);
+    u = read(f, s, 1);
+    int j = 0;
 
-    read(f, t, l);
-
+    while(u > 0 && j < c) {
+        if(s[0] == '.' || s[0] == '#' || s[0] == '\n') {
+            r[j] = s[0];
+            ++j;
+        }
+        u = read(f, s, 1);
+        --j;
+        j++;
+    }
+    r[c] = '\0';
     close(f);
-    return t;
+    return r;
 }
 
