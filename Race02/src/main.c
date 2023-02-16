dc407c9ef4e38f3bd0145d2e3b80b7e9b5c76553
Нікулін Артем Дмитрович <48065996+lerobait@users.noreply.github.com>
2023-02-16 13:41:47 +0200

    Race02

--- Race02/src/main.c
+++ Race02/src/main.c
@@ -4,5 +4,17 @@ int main(int argc, char const *argv[]) {
     if(argc != 6) {
         mx_printerr("usage: ./way_home [file_name] [x1] [y1] [x2] [y2]");
     }
+    char s[1];
+    int f = open(argv[1], O_READONLY);
+    int u = read(f, s, sizeof(s));
+
+    if(f < 0) {
+        mx_printerr("map does not exist\n");
+        exit(0);
+    }
+    else if(u <= 0) {
+        mx_printerr("map does not exist\n");
+        exit(0);
+    }
 }
 
